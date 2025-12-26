5cf54420205d1a6c746d46059bd4bacddad71bf9
vijenderyuadav944 <vijenderyadav944@gmail.com>
2025-12-26 12:15:00 +0530

    deployment error 500

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
-import { authConfig } from "@/lib/auth";
+import { authConfigEdge } from "@/lib/auth";
 
-export default NextAuth(authConfig).auth;
+export default NextAuth(authConfigEdge).auth;
 
 export const config = {
   // Exclude public assets, public pages.
